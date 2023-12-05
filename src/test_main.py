da671a51e14e5d16f7c4ee065d801606560a1ba9
alina <146937991+heyitsalina@users.noreply.github.com>
2023-12-05 21:12:12 +0100

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,1 +1,6 @@
-hihi
+from main.py import fair_sharer
+
+# Unit Testing
+def test_fair_sharer():
+    assert fair_sharer([0,1000,800,0], 1) == [100, 800, 900, 0]      # checking, if it is the right output
+    assert fair_sharer([0, 1000, 800, 0], 2) == [100, 890, 720, 90]
